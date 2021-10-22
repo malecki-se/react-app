409579b2a73bb287cd736d7a22ff14547857cea8
sebastian_malecky <sebastian.malecky@gmail.com>
2021-10-22 22:00:57 +0200

    sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://85f09c00976b442eadba99f61d8e1c78@o1048862.ingest.sentry.io/6030239",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
